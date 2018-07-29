19d0220453111883dfa740e1cb88fac89f2b8a41
Chris Barran <chris.d.barran@gmail.com>
2018-07-29 09:47:07 +0100

    Try multi stage

--- pipelines/pipeline1.groovy
+++ pipelines/pipeline1.groovy
@@ -1,5 +1,11 @@
 node ('master'){
     stage('Hello') {
+        echo "Hello "
+    }
+    stage('World') {
+        echo "World!"
+    }
+    stage('Data') {
         try {
             echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
         } catch (exc) {
