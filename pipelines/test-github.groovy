bd41affa44c564dd038addf404060b227d749940
Chris Barran <chris.d.barran@gmail.com>
2018-07-29 11:35:54 +0100

    Added Pipeline job to test github api

---
+++ pipelines/test-github.groovy
@@ -0,0 +1,34 @@
+def FAILURE_EMAIL_RECIPIENTS = "***REMOVED***"
+def apiURL = "https://status.github.com/api/last-message.json"
+
+// Maximum time for the whole curl operation to take before failing (seconds).
+def maxTime = 10
+
+node('master') {
+
+    try {
+        def jsonText
+        
+        stage('API Reacheable') {
+            jsonText = sh (script: "curl --silent --show-error --fail -m ${maxTime} ${apiURL}", returnStdout: true).trim()
+        }
+
+        stage('API Status Good') {
+            validateStatus(jsonText)
+            currentBuild.result = 'SUCCESS'
+        }
+    } catch (any) {
+        currentBuild.result = 'FAILURE'
+        throw any
+    } finally {
+        step([$class: 'Mailer', notifyEveryUnstableBuild: false, recipients: FAILURE_EMAIL_RECIPIENTS, sendToIndividuals: true ])
+    }
+}
+
+def validateStatus(message){
+    def jsonSlurper = new groovy.json.JsonSlurper()
+    def json = jsonSlurper.parseText(message)
+    echo ("GitHub Status: ${json.status} - ${json.body}")
+    assert json.status == "good"
+}
+
