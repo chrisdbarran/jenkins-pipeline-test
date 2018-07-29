184e94fe40f1678d699aed6da16f45c4fbf88623
Chris Barran <chris.d.barran@gmail.com>
2018-07-29 09:18:11 +0100

    Testing simpler script

--- pipelines/pipeline1.groovy
+++ pipelines/pipeline1.groovy
@@ -1,11 +1,3 @@
 node {
-    stage('Hello') {
-        echo('Hello ')
-    }
-    stage('World!') {
-        echo('World!')
-    }
-    stage('Data') {
-       echo('Running ${env.BUILD_ID} on ${env.JENKINS_URL}') 
-    }
+    echo('Running ${env.BUILD_ID} on ${env.JENKINS_URL}') 
 }
